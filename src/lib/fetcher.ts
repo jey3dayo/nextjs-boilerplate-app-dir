dc83d62beec63ec2c9385d26ecd275e0950a40e3
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-05-12 07:10:24 +0000

    fetchをcatchするとbuildエラーが出るので対策

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -3,9 +3,5 @@ import { baseUrl } from '@/constants';
 export const forge = (url: string) => (/^http/.test(url) ? url : `${baseUrl}${url}`);
 
 export async function fetcher(url: string, init?: RequestInit) {
-  return fetch(forge(url), init)
-    .then(res => res.json() as Promise<any | null>)
-    .catch(err => {
-      console.error(err);
-    });
+  return fetch(forge(url), init).then(res => res.json() as Promise<any | null>);
 }
