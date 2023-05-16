640eef53ac72e34b5ff56b7cf59ec42a5990038d
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-05-16 08:45:56 +0000

    link修正

--- src/components/header/constants.tsx
+++ src/components/header/constants.tsx
@@ -5,10 +5,9 @@ type Navigation = {
 };
 
 export const navigation: Navigation[] = [
-  { name: 'example', href: '/example', current: true },
-  { name: 'example1', href: '/example1', current: false },
-  { name: 'example2', href: '/example2', current: false },
-  { name: 'example3', href: '/example3', current: false },
+  { name: 'example1', href: '#example1', current: false },
+  { name: 'example2', href: '#example2', current: false },
+  { name: 'example3', href: '#example3', current: false },
 ];
 
 export const profileNavigation: Navigation[] = [
