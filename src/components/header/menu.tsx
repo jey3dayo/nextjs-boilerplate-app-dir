718a9c9727f07f8cead0837a23e2f92e19755198
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-05-17 07:18:24 +0000

    navigation整理

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -3,7 +3,7 @@ import { Menu, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { Fragment } from 'react';
-import { profileNavigation } from '@/components/header/constants';
+import { profileNavigation as navigation } from '@/components/header/constants';
 import classNames from '@/lib/class-names';
 
 export default function CustomMenu({ children }: ReactProps) {
@@ -28,24 +28,26 @@ export default function CustomMenu({ children }: ReactProps) {
         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
           <div className="p-1 ">
             {user ? (
-              <Menu.Item>
-                <div className={classNames('', 'block px-4 py-1 text-sm text-gray-700')}>{user?.name}</div>
-              </Menu.Item>
+              <>
+                <Menu.Item>
+                  <div className={classNames('', 'block px-4 py-1 text-sm text-gray-700')}>{user?.name}</div>
+                </Menu.Item>
+                <hr />
+                {navigation.map((v, i) => (
+                  <Menu.Item key={i}>
+                    {({ active }) => (
+                      <Link
+                        href={v.href}
+                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                      >
+                        {v.name}
+                      </Link>
+                    )}
+                  </Menu.Item>
+                ))}
+              </>
             ) : null}
 
-            <hr />
-            {profileNavigation.map((v, i) => (
-              <Menu.Item key={i}>
-                {({ active }) => (
-                  <Link
-                    href={v.href}
-                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                  >
-                    {v.name}
-                  </Link>
-                )}
-              </Menu.Item>
-            ))}
             <Menu.Item>
               {({ active }) =>
                 user ? (
