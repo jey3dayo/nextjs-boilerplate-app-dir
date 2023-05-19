78d4ca67f28ba15b05fd1c5d3b040df668eb91f8
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-05-19 07:47:02 +0000

    scheme整理

--- prisma/migrations/20230519074131_init/migration.sql
+++ prisma/migrations/20230519074131_init/migration.sql
@@ -4,10 +4,11 @@ CREATE TABLE "User" (
     "provider" VARCHAR(20) NOT NULL,
     "cynumber" VARCHAR(50) NOT NULL,
     "email" VARCHAR(254) NOT NULL,
-    "familyName" VARCHAR(255) NOT NULL,
-    "givenName" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "picture" VARCHAR(2048) NOT NULL,
-    "roleId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -17,12 +18,11 @@ CREATE TABLE "Role" (
     "id" INTEGER NOT NULL,
     "name" VARCHAR(10) NOT NULL,
     "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,35 +3,31 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const role1 = await prisma.role.upsert({
+  await prisma.role.upsert({
     where: { id: 1 },
     update: {},
     create: { id: 1, name: 'user', description: '一般ユーザー' },
   });
-  console.log('Created roles:', role1);
-
-  const role2 = await prisma.role.upsert({
+  await prisma.role.upsert({
     where: { id: 2 },
     update: {},
     create: { id: 2, name: 'admin', description: '管理者' },
   });
-  console.log('Created roles:', role2);
 
-  const user1 = await prisma.user.upsert({
-    where: { id: 'oauth2|CASSO|T00114' },
+  const user = await prisma.user.upsert({
+    where: { id: 'oauth2|CASSO|A00000' },
     update: {},
     create: {
-      id: 'oauth2|CASSO|T00114',
+      id: 'oauth2|CASSO|A00000',
       provider: 'CASSO',
-      cynumber: 'T00114',
-      familyName: '仲里',
-      givenName: '淳矢',
-      email: 'nakazato_junya@ca-adv.co.jp',
+      cynumber: 'A00000',
+      name: '斎場 太郎',
+      email: 'example@example.com',
       picture: '',
       roleId: 1,
     },
   });
-  console.log('Created user:', user1);
+  console.log('Created user:', user);
 }
 
 main()
