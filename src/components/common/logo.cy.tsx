3fbcd5117c36a1ad5605be0e2cebc533bd61b9c3
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-05-23 08:43:47 +0000

    cypress向けのテスト試しで実装

---
+++ src/components/common/logo.cy.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import Logo from './logo';
+
+describe('<Logo />', () => {
+  beforeEach(() => {
+    cy.mount(<Logo />);
+  });
+
+  it('should render the logo image with correct alt text', () => {
+    cy.get('img[alt="logo"]').should('be.visible');
+  });
+
+  it('should have correct width and height', () => {
+    cy.get('img[alt="logo"]').should('have.attr', 'width', '100');
+    cy.get('img[alt="logo"]').should('have.attr', 'height', '80');
+  });
+});
+
+export {};
--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import logo from '@/../public/dalle-cap.svg';
+import logo from 'public/dalle-cap.svg';
 
 export default function Logo({ width, height }: { width?: string; height?: string }) {
   return <Image src={logo} alt="logo" width={Number(width ?? 100)} height={Number(height ?? 80)} priority />;
