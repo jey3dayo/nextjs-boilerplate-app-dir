fecae7033da9686428f0e6212366c4c56fbc7c88
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-06-19 15:32:56 +0900

    borderをカスタムテーマへ

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { env } from '@/env.mjs';
 
 export default function Footer() {
   return (
-    <footer className="flex h-[4vh] min-h-[1.5rem] items-center justify-center border-t border-dark/20 dark:border-light/10">
+    <footer className="theme-border flex h-[4vh] min-h-[1.5rem] items-center justify-center">
       <span className="text-sm text-dark dark:text-medium">
         <span className="px-0.5">©</span>
         <span>2023 {env.NEXT_PUBLIC_APP_NAME ?? ''}, Inc.</span>
