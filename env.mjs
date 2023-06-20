ccc79620e12e39a998cf4c15abb348432712a62f
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-06-20 15:23:21 +0900

    installed auth0Provider

--- env.mjs
+++ env.mjs
@@ -7,6 +7,9 @@ export const env = createEnv({
     VERCEL_URL: z.string().url().optional(),
     GITHUB_ID: z.string().min(1),
     GITHUB_SECRET: z.string().min(1),
+    AUTH0_CLIENT_ID: z.string().min(1),
+    AUTH0_CLIENT_SECRET: z.string().min(1),
+    AUTH0_ISSUER: z.string().url(),
     NEXTAUTH_SECRET: z.string().min(1),
 
     // Not needed on VERCEL
@@ -24,6 +27,9 @@ export const env = createEnv({
     BASE_URL: process.env.BASE_URL,
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_ISSUER: process.env.AUTH0_ISSUER,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXT_PUBLIC_APP_NAME: process.env?.NEXT_PUBLIC_APP_NAME,
--- lib/next-auth/index.ts
+++ lib/next-auth/index.ts
@@ -1,26 +1,36 @@
 import 'server-only';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextAuthOptions } from 'next-auth';
+import Auth0Provider from 'next-auth/providers/auth0';
 import GithubProvider from 'next-auth/providers/github';
 import { OAuthConfig } from 'next-auth/providers/oauth';
 import { env } from '@/env.mjs';
 import CassoProvider from '@/lib/next-auth/casso-provider';
 import { prismaClient } from '@/lib/prisma';
 
+let providers: OAuthConfig<any>[] = [];
+
 // api/auth/callback/casso
 const cassoProvider = CassoProvider({
   clientId: env?.CASSO_CLIENT_ID as string,
   clientSecret: env?.CASSO_CLIENT_SECRET as string,
 });
+if (env?.CASSO_CLIENT_ID) providers.push(cassoProvider);
 
 // api/auth/callback/github
 const githubProvider = GithubProvider({
   clientId: env.GITHUB_ID as string,
   clientSecret: env.GITHUB_SECRET as string,
 });
+if (env?.GITHUB_ID) providers.push(githubProvider);
 
-let providers: OAuthConfig<any>[] = [githubProvider];
-if (env?.CASSO_CLIENT_ID) providers.push(cassoProvider);
+// api/auth/callback/auth0
+const auth0Provier = Auth0Provider({
+  clientId: env.AUTH0_CLIENT_ID as string,
+  clientSecret: env.AUTH0_CLIENT_SECRET as string,
+  issuer: process.env.AUTH0_ISSUER as string,
+});
+if (env?.AUTH0_CLIENT_ID) providers.push(auth0Provier);
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prismaClient),
