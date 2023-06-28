b21e68fbe76a47c52789be5a069dbc723ef02c1a
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-06-28 11:22:09 +0000

    refactor: extract  providers.ts

---
+++ lib/next-auth/provider.ts
@@ -0,0 +1,31 @@
+import Auth0Provider from 'next-auth/providers/auth0';
+import GithubProvider from 'next-auth/providers/github';
+import { OAuthConfig } from 'next-auth/providers/oauth';
+import { env } from '@/env.mjs';
+import { CassoProvider } from '@/lib/next-auth/casso-provider';
+
+let providers: OAuthConfig<any>[] = [];
+
+// api/auth/callback/casso
+const cassoProvider = CassoProvider({
+  clientId: env?.CASSO_CLIENT_ID as string,
+  clientSecret: env?.CASSO_CLIENT_SECRET as string,
+});
+if (env?.CASSO_CLIENT_ID) providers.push(cassoProvider);
+
+// api/auth/callback/github
+const githubProvider = GithubProvider({
+  clientId: env.GITHUB_ID as string,
+  clientSecret: env.GITHUB_SECRET as string,
+});
+if (env?.GITHUB_ID) providers.push(githubProvider);
+
+// api/auth/callback/auth0
+const auth0Provier = Auth0Provider({
+  clientId: env.AUTH0_CLIENT_ID as string,
+  clientSecret: env.AUTH0_CLIENT_SECRET as string,
+  issuer: process.env.AUTH0_ISSUER as string,
+});
+if (env?.AUTH0_CLIENT_ID) providers.push(auth0Provier);
+
+export { providers };
