91d204c0675db4b63d2efa7febbb0ab5b0e18c11
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-07-07 19:25:16 +0000

    refactor: removed styles.module.css

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -2,10 +2,17 @@
 
 import { Bars } from 'react-loader-spinner';
 import { color } from '@/styles/colors';
-import styles from '@/styles/home.module.css';
 
 const size = 80;
 
 export function Loader() {
-  return <Bars height={size} width={size} color={color.dark} ariaLabel="読込中" wrapperClass={styles.main} />;
+  return (
+    <Bars
+      height={size}
+      width={size}
+      color={color.dark}
+      ariaLabel="読込中"
+      wrapperClass="flex min-h-screen flex-1 flex-col items-center px-4 min-height-[100vh]"
+    />
+  );
 }
