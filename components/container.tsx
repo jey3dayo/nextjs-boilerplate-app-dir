4502802597de20e4f6f8efa228da11987bf452d5
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-07-13 18:19:10 +0000

    style(components): update container component styles

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,15 @@
 import { classNames } from '@/lib/class-names';
 
 export function Container(props: ReactProps) {
-  return <div className={classNames('isolate flex overflow-hidden', props.className)}>{props.children}</div>;
+  return (
+    <div className={classNames('relative isolate overflow-hidden px-1 py-0 md:py-2 lg:px-8', props.className)}>
+      {props.children}
+    </div>
+  );
 }
 
 export function HeroContainer(props: ReactProps) {
-  return <div className={classNames('isolate flex grow overflow-hidden', props.className)}>{props.children}</div>;
+  return (
+    <div className={classNames('container isolate flex grow overflow-hidden', props.className)}>{props.children}</div>
+  );
 }
