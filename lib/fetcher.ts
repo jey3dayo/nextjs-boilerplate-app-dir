f1fcde9e59533e44d19bdb43e7e46aa183bcc9a6
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-07-13 09:17:20 +0000

    fix(fetcher): use smartFetch in fetcher function

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -2,6 +2,10 @@ import { baseUrl } from '@/constants/api';
 
 export const forge = (url: string) => (/^http/.test(url) ? url : `${baseUrl}${url}`);
 
+export async function smartFetch(url: string, init?: RequestInit) {
+  return fetch(forge(url), init);
+}
+
 export async function fetcher(url: string, init?: RequestInit) {
-  return fetch(forge(url), init).then((res) => res.json() as Promise<any | null>);
+  return smartFetch(url, init).then((res) => res.json());
 }
