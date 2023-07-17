f8b36ce871d2e60c289b901c084335f98b95eb01
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-07-17 16:36:40 +0000

    fix(fetcher): handle API request errors and internal server errors

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,7 @@
 import { baseUrl } from '@/constants/api';
+import { messages } from '@/constants/messages';
 import { getAllCookies } from '@/lib/api-utils';
+import { ApiRequestError } from '@/lib/error';
 
 export const forge = (url: string) => (/^http/.test(url) ? url : `${baseUrl}${url}`);
 
@@ -18,9 +20,19 @@ export async function fetcher(url: string, init?: RequestInit) {
       ...init,
     } as RequestInit;
     const res = await smartFetch(url, options);
-    if (res.ok) {
-      return res.json();
+
+    if (!res.ok) {
+      const json = await res.json();
+
+      if (json.error) {
+        const error = new ApiRequestError(json.error, res.status);
+        throw error;
+      } else {
+        throw new Error(messages.internalServerError);
+      }
     }
+
+    return res.json();
   } catch (e) {
     throw e;
   }
