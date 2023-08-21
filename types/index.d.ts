1e23edf653fcd37fc9f1caeee711d582c1fe7f6e
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-08-21 09:42:16 +0000

    feat(types): add support for image file types

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,8 +13,3 @@ declare module 'next/app' {
     Component: NextPageWithLayout<P>;
   };
 }
-
-declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGElement>>;
-  export default content;
-}
