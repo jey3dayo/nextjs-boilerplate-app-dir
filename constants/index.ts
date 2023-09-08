36ddc436c78df72bfa5f631b09033aa3dbcd0629
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-09-08 09:48:13 +0000

    feat: add docs to navigation

--- constants/index.ts
+++ constants/index.ts
@@ -8,6 +8,7 @@ export const titles = {
   sample: 'サンプル',
   sampleAdmin: 'サンプルAdmin',
   users: 'ユーザ管理',
+  docs: 'ドキュメント',
   errorPage: 'エラーページ',
 };
 
@@ -21,6 +22,7 @@ type Navigation = {
 export const navigation: Navigation[] = [
   { title: titles.dashboard, href: '/dashboard', requiredAdmin: false, current: false },
   // { name: titles.admin, href: '/admin', requiredAdmin: true, current: false },
+  { title: titles.docs, href: '/docs', requiredAdmin: false, current: false },
 ];
 
 export const profileNavigation: Navigation[] = [
