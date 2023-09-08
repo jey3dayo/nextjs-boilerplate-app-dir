df5e78c1c16e31c3c6f4b8f786e0db0ada9e69cf
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2023-09-08 01:43:44 +0000

    fix(date): Handle invalid date input and formatting errors

--- lib/date.ts
+++ lib/date.ts
@@ -1,6 +1,8 @@
 import fromUnixTime from 'date-fns/fromUnixTime';
 import isAfter from 'date-fns/isAfter';
 
+const INVALID_DATE_MESSAGE = 'Invalid Date';
+
 export function isExpired(exp: number | undefined): boolean {
   if (exp === undefined) return false;
 
@@ -11,9 +13,20 @@ export function isExpired(exp: number | undefined): boolean {
 
 export function formatDate(input: string | number): string {
   const date = new Date(input);
-  return date.toLocaleDateString('ja-JP', {
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date input');
+  }
+
+  const formattedDate = date.toLocaleDateString('ja-JP', {
     month: 'long',
     day: 'numeric',
     year: 'numeric',
   });
+
+  if (formattedDate === INVALID_DATE_MESSAGE) {
+    throw new Error('Failed to format date');
+  }
+
+  return formattedDate;
 }
